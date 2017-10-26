38799ef9301272a1873276d705b5e952818ae86f
johnpaulsmith87 <johnpaul.smith@gmail.com>
2017-10-26 13:10:48 +1000

    progress commit

--- src/assignment3/Controller.java
+++ src/assignment3/Controller.java
@@ -3,16 +3,73 @@ package assignment3;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 /**
  * Controller class for the Character Editor.
+ * 
  * @author leggy (Lachlan Healey)
  */
-public class Controller implements Initializable{
-	
-	private Model model;	
-	
+public class Controller implements Initializable {
+
+	private Model model;
+
+	// text fields
+	@FXML
+	private TextField txtCreateDatabase;
+	@FXML
+	private TextField txtCreateCharacterName;
+	@FXML
+	private TextField txtSearchCharacter;
+	@FXML
+	private TextField txtDescription;
+	@FXML
+	private TextField txtPowerLevel;
+	// text areas
+	@FXML
+	private TextArea txtTraits;
+	@FXML
+	private TextArea txtPowers;
+	// buttons
+	@FXML
+	private Button btnSaveDatabase;
+	@FXML
+	private Button btnLoadDatabase;
+	@FXML
+	private Button btnCreateDatabase;
+	@FXML
+	private Button btnCreateCharacter;
+	@FXML
+	private Button btnCreateSuperCharacter;
+	@FXML
+	private Button btnSearchCharacter;
+	@FXML
+	private Button btnClearSearch;
+	@FXML
+	private Button btnDeleteCharacter;
+	@FXML
+	private Button btnChangeImage;
+	@FXML
+	private Button btnSaveCharacter;
+	// imageview
+	@FXML
+	private ImageView imvCharacterImage;
+	@FXML
+	private Label lblCharacterName;
+	// listview
+	@FXML
+	private ListView<String> lsvViewDatabase;
+
 	public Controller() {
 		/*
 		 * TODO: DELETE THIS COMMENT AND INITIALISE YOUR CONTROLLER HERE
@@ -22,9 +79,26 @@ public class Controller implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		/*
-		 * TODO: DELETE THIS COMMENT AND INITIALISE YOUR GUI HERE
-		 */
+		handleClearSearchAction();
+		handleSearchCharacterAction();
+		
+	}
+
+	private void handleClearSearchAction() {
+		btnClearSearch.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//add code here!
+			}
+		});
+	}
+	private void handleSearchCharacterAction() {
+		btnSearchCharacter.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				//add code here!
+			}
+		});
 	}
 
 }
--- src/assignment3/Model.java
+++ src/assignment3/Model.java
@@ -9,13 +9,16 @@ public class Model {
 	/*
 	 * TODO: DELETE THIS LINE AND ADD INSTANCE VARIABLES
 	 */
-	
+	private Character selectedCharacter;
+	private CharacterDatabase activeDatabase;
+	private CharacterDatabaseCreator databaseCreator;
 	/*
 	 * TODO: INVARIANT GOES HERE
 	 */
 	
 	public Model() {
 		/*
+		 * This conatins: database and currently selected character
 		 * TODO: DELETE THIS LINE AND IMPLEMENT CONSTRUCTOR
 		 */
 	}
